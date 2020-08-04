9a7aaaf781e5addd1b094de452b7aa887067cf3e
dimitrius-brest <d_der@mail.ru>
2020-08-04 12:50:36 +0300

    created

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mogoose = require('mongoose');
+const path = require('path');
+const exphbs = require('express-handlebars');
+const todoRoutes = require('./routes/todos');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+});
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(todoRoutes);
+
+async function start() {
+    try {
+        await mogoose.connect('mongodb+srv://user:1q2w3e4r@cluster0.tefog.mongodb.net/todos', {
+            useNewUrlParser: true,
+            useFindAndModify: false
+        });
+        app.listen(PORT, () => {
+            console.log('Server has been started...')
+        });
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+start();
